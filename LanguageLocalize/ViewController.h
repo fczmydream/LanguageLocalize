df5429ed2c396f2aef085f35c3de748180044871
fcz <1607951422@qq.com>
2017-06-05 15:10:39 +0800

    Initial Commit

---
+++ LanguageLocalize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LanguageLocalize
+//
+//  Created by fcz on 2017/6/5.
+//  Copyright © 2017年 com.labang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LanguageLocalizeTests/LanguageLocalizeTests.m
@@ -0,0 +1,39 @@
+//
+//  LanguageLocalizeTests.m
+//  LanguageLocalizeTests
+//
+//  Created by fcz on 2017/6/5.
+//  Copyright © 2017年 com.labang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LanguageLocalizeTests : XCTestCase
+
+@end
+
+@implementation LanguageLocalizeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
